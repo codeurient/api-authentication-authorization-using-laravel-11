1d4ce1e24e4c13d20183d6078b243f6c7dd9bd83
codeurient <codeurient@gmail.com>
2024-12-08 22:17:08 +0400

    validator

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,27 +5,42 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
     public function register(Request $request) {
-        $validated = $request->validate([
+//        $validated = $request->validate([
+//            'name' => 'required|string|max:255',
+//            'email' => 'required|string|email|max:255|unique:users',
+//            'password' => 'required|string|min:6|confirmed',
+//        ]);
+
+        $validated = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
         ]);
 
-        $user = User::create([
-            'name' => $validated['name'],
-            'email' => $validated['email'],
-            'password' => Hash::make($validated['password']),
-        ]);
+        if ($validated->fails()) {
+            return response()->json($validated->errors(), 422);
+        }
 
-        $token = $user->createToken('auth_token')->plainTextToken;
+        try {
+            $user = User::create([
+                'name' => $request->name,
+                'email' =>  $request->email,
+                'password' => Hash::make($request->password),
+            ]);
 
-        return response()->json([
-            'access_token' => $token,
-            'user' => $user,
-        ], 200);
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'access_token' => $token,
+                'user' => $user,
+            ], 200);
+        } catch (\Exception $exception) {
+            return response()->json(['error' => $exception->getMessage()], 403);
+        }
     }
 }
