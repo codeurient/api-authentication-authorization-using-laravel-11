1f23ac5a4db3e45c3964a702bda8b34f53c21ff8
codeurient <codeurient@gmail.com>
2024-12-09 08:22:27 +0400

    addNewPost

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
-    //
+    public function addNewPost(Request $request)
+    {
+        $validated = Validator::make($request->all(), [
+            'title' => 'required|string',
+            'content' => 'required|string',
+        ]);
+
+        if ($validated->fails()) {
+            return response()->json($validated->errors(), 422);
+        }
+
+        try {
+            $post = new Post();
+            $post->title = $request->title;
+            $post->content = $request->content;
+            $post->user_id = auth()->user()->id;
+            $post->save();
+
+            return response()->json([
+                'message' => 'Post added successfully',
+                'post_data' => $post,
+            ], 200);
+
+        } catch (\Exception $th) {
+            return response()->json(['error' => $th->getMessage()], 403);
+        }
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -25,4 +26,7 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function() {
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    // blog api
+    Route::post('/add/post', [PostController::class, 'addNewPost']);
 });
