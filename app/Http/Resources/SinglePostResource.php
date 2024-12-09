ba8c47d586dbd54899395be76576a22d5a8bc0f3
codeurient <codeurient@gmail.com>
2024-12-09 16:25:29 +0400

    single post resource fixed

--- app/Http/Resources/SinglePostResource.php
+++ app/Http/Resources/SinglePostResource.php
@@ -3,7 +3,9 @@
 namespace App\Http\Resources;
 
 
-use Illuminate\Http\Request;
+use App\Models\Comment;
+use App\Models\Like;
+use App\Models\User;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class SinglePostResource extends JsonResource
@@ -16,12 +18,18 @@ class SinglePostResource extends JsonResource
     public function toArray($request): array
     {
         return [
-            'id'            => $this->id,
-            'post_title'    => $this->title,
-            'post_content'  => $this->content,
-            'author_id'     => $this->user_id,
-            'published_at'  => $this->created_at,
-            'last_update'   => $this->updated_at,
+            'id'                => $this->id,
+            'post_title'        => $this->title,
+            'post_content'      => $this->content,
+            'author_id'         => $this->user_id,
+            'published_at'      => $this->created_at,
+            'last_update'       => $this->updated_at,
+            'author'            => User::find($this->user_id),
+            // 'comment_count'     => Comment::find($this->id)->count(),
+            // 'comments'          => Comment::find($this->id),
+            'comment_count'     => Comment::where('post_id', $this->id)->count(), 
+            'likes_count'       => Like::where('post_id', $this->id)->where('user_id', $this->user_id)->count(), 
+            'comments'          => Comment::where('post_id', $this->id)->get(),  
         ];
     }
 }
